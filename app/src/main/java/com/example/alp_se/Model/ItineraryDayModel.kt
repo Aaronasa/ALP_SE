c88e814230935e8d80f7cba472f5c1458142b4f1
marcellino Indra Wijaya <marcellinowijaya39@gmail.com>
2025-05-29 16:55:46 +0700

    push

--- app/src/main/java/com/example/alp_se/Model/ItineraryDayModel.kt
+++ app/src/main/java/com/example/alp_se/Model/ItineraryDayModel.kt
@@ -1,4 +1,39 @@
 package com.example.alp_se.Model
 
-class ItineraryDayModel {
-}
+data class GetAllItineraryDayResponse(
+    val data: List<ItineraryDayModel>
+)
+
+
+data class ItineraryDayModel(
+    val id: Int = 0,
+    val day: String = "",
+    val start_time: String = "",
+    val end_time: String = "",
+    val activity_description: String = "",
+    val meeting_time: String = "",
+    val itineraryId: Int = 0
+)
+
+data class CreateItineraryDayRequest(
+    val day: String,
+    val start_time: String,
+    val end_time: String,
+    val activity_description: String,
+    val meeting_time: String,
+    val itineraryId: Int
+)
+
+data class UpdateItineraryDayRequest(
+    val id: Int,
+    val day: String? = null,
+    val start_time: String? = null,
+    val end_time: String? = null,
+    val activity_description: String? = null,
+    val meeting_time: String? = null,
+    val itineraryId: Int? = null
+)
+
+data class DeleteItineraryDayRequest(
+    val id: Int
+)
--- app/src/main/java/com/example/alp_se/Service/ItineraryDayService.kt
+++ app/src/main/java/com/example/alp_se/Service/ItineraryDayService.kt
@@ -1,4 +1,35 @@
 package com.example.alp_se.Service
 
-class ItineraryDayService {
+import com.example.alp_se.Model.CreateItineraryDayRequest
+import com.example.alp_se.Model.GetAllItineraryDayResponse
+import com.example.alp_se.Model.GeneralResponseModel
+import com.example.alp_se.Model.UpdateItineraryDayRequest
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
+
+interface ItineraryDayService {
+    @GET("/itineraryDays")
+    suspend fun getAllItineraryDays(): Response<GetAllItineraryDayResponse>
+
+    @POST("/itineraryDays")
+    suspend fun createItineraryDay(
+        @Body createItineraryDayRequest: CreateItineraryDayRequest
+    ): Response<GeneralResponseModel>
+
+
+    @PUT("/itineraryDays/{id}")
+    suspend fun updateItineraryDay(
+        @Path("id") id: Int,
+        @Body updateItineraryDayRequest: UpdateItineraryDayRequest
+    ): Response<GeneralResponseModel>
+
+    @DELETE("/itineraryDays/{id}")
+    suspend fun deleteItineraryDay(
+        @Path("id") id: Int
+    ): Response<GeneralResponseModel>
 }
