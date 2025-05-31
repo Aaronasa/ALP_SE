dfe75e960a689f7acd5a7934d4d353da84d73e55
davetanjung <davegtanjung@gmail.com>
2025-05-31 08:23:26 +0700

    push

--- app/src/main/java/com/example/alp_se/Route/AppRouting.kt
+++ app/src/main/java/com/example/alp_se/Route/AppRouting.kt
@@ -7,11 +7,19 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.alp_se.AppContainer
+import com.example.alp_se.View.CreateItineraryView
 import com.example.alp_se.View.HomeView
+import com.example.alp_se.View.ListItineraryDayView
 import com.example.alp_se.View.ListItineraryView
+import com.example.alp_se.ViewModel.ItineraryViewModel
 
 enum class listScreen(){
     HomeView,
@@ -24,7 +32,10 @@ enum class listScreen(){
 }
 
 @Composable
-fun AppRouting() {
+fun AppRouting(
+    itineraryViewModel: ItineraryViewModel = viewModel(factory = ItineraryViewModel.Factory),
+    navController: NavController? = null
+) {
     val NavController = rememberNavController()
 
     Scaffold { innerPadding ->
@@ -34,18 +45,49 @@ fun AppRouting() {
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(route = listScreen.HomeView.name) {
-                HomeView(onSplashFinish = {
-                    NavController.navigate(listScreen.HomeView.name) {
-                        popUpTo(listScreen.HomeView.name) { inclusive = true }
-                    }
-                })
+                HomeView(
+                    onSplashFinish = {
+                        NavController.navigate(listScreen.HomeView.name) {
+                            popUpTo(listScreen.HomeView.name) { inclusive = true }
+                        }
+                    },
+                    navController = NavController
+                )
             }
 
             composable(
                 route = listScreen.ListItineraryView.name,
             ) {
-                ListItineraryView()
+                ListItineraryView(
+                    navController = NavController,
+                    itineraryViewModel = itineraryViewModel
+                )
             }
+
+            composable(
+                route = listScreen.CreateItineraryView.name,
+            ) {
+                CreateItineraryView(
+                    navController = NavController,
+                    itineraryViewModel = itineraryViewModel
+                )
+            }
+
+//            composable(
+//                route = listScreen.ListItineraryDayView.name + "/{itinerary_id}",
+//                arguments = listOf(
+//                    navArgument("itinerary_id") { type = NavType.IntType }
+//                )
+//            ) { backStackEntry ->
+//                val itinerary_id = backStackEntry.arguments?.getInt("itinerary_id")
+//                requireNotNull(itinerary_id) { "itinerary_id is required to navigate to ListItineraryDayView" }
+//
+//                ListItineraryDayView(
+//                    navController = NavController,
+//                    // itineraryDayViewModel, // You'll need to add this parameter if required
+//                    itinerary_id
+//                )
+//            }
         }
     }
 }
