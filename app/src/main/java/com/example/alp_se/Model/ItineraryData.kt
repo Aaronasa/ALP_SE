6f8b370d742a474918ef18d308f2a11576eb2460
davetanjung <davegtanjung@gmail.com>
2025-06-06 22:51:01 +0700

    push

--- app/src/main/java/com/example/alp_se/Model/ItineraryData.kt
+++
@@ -1,9 +0,0 @@
-package com.example.alp_se.Model
-
-data class ItineraryData(
-    val dayTitle: String,
-    val meetingTime: String,
-    val sneakPeekDesc: String,
-    val location: String? = null,
-    val isCompleted: Boolean = false
-)
